2f50b61c98d2dd6ed94ad28254c1d8dff40e58d0
Black-Stormy <brookesy@internode.on.net>
2017-07-29 22:03:06 +1000

    first attempt at slingshot mechanic

---
+++ Assets/Manager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour {
+	
+	// public gameobject variables
+	public Camera cam;
+
+	// internal gameobject references
+
+	// public variables
+
+	// private variables
+	private string logStr = "eyy";
+
+
+	// Use this for initialization
+	void Start () {
+		/*if( cam == null ) {
+			Debug.Log("camera not found");
+		} else {
+			Debug.Log("camera found");
+		}*/
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// check for click, log mouse position
+		if (Input.GetMouseButton (0)) {
+			logStr = Input.mousePosition.ToString ();
+		}
+
+		
+	}
+
+	void OnGUI() {
+		GUI.Box (new Rect (100, 100, 100, 20), logStr);
+	}
+
+}
---
+++ Assets/flingyBall.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(MeshCollider))]
+
+
+public class flingyBall : MonoBehaviour 
+{
+
+	public Camera cam;
+	public GameObject pivotPoint;
+	public float forceMultiplier = 500;
+
+	private GameObject springBase;
+	private SpringJoint springy;
+	private Rigidbody rb;
+	private Rigidbody pivrb; // pivot
+
+	private Vector3 screenPoint;
+	private Vector3 offset;
+	private Vector3 springVec;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+		pivrb = pivotPoint.GetComponent<Rigidbody>();
+
+	}
+	void Update()
+	{
+		if (Input.GetMouseButtonDown (0)) {
+			// clear all force acting on the gameobject
+			rb.velocity = Vector3.zero;
+			rb.angularVelocity = Vector3.zero;
+
+		}
+		if (Input.GetMouseButtonUp (0)) {
+			// launch the sphere
+			//Debug.Log(springVec);
+			rb.AddForce(Vector3.Scale(springVec,new Vector3(forceMultiplier,forceMultiplier,forceMultiplier)));
+			
+		}
+		if (Input.GetMouseButton (0)) {
+
+			// grab the ball
+			// find screen co-ords of touch
+			Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10);
+			// convert screen co-ords to a position in the world (the z of which is controlled ^)
+			Vector3 curPosition = cam.ScreenToWorldPoint(curScreenPoint);
+			// get the vector between in-world touch location and pivot point which we place in editor
+			Vector3 curPivot =  pivrb.position - curPosition;
+			// clamp the magnitude so our ball can only go so far back
+			Vector3 ballClamp = pivrb.position - Vector3.ClampMagnitude(curPivot, 10.0f);
+			// record the vector between our clamped ball and the pivot point
+			// we'll use this on release to determine the shot
+			springVec = Vector3.ClampMagnitude (curPivot, 10.0f);
+			rb.position = ballClamp;
+		}
+	}
+
+
+}
