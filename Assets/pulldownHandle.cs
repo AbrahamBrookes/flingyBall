cf9646138dd06d0ba976e459f1a81bdc3d6b18be
Black-Stormy <brookesy@internode.on.net>
2017-09-14 20:24:23 +1000

    menu mechanically functional

--- Assets/pulldownHandle.cs
+++ Assets/pulldownHandle.cs
@@ -5,71 +5,78 @@ using UnityEngine;
 public class pulldownHandle : MonoBehaviour {
 
 	public GameObject menuItself;
-	public float menuPullRatio = 1.25f;
-	public float menuOffset = 0.75f;
-	public float handleUpperLimit = 0.575f;
-	public float handleLowerLimit = 0.4f;
 
-	private GameObject spawnTest;
-	private Vector3 wrldTouch;
-	public Camera camCam;
-	private Vector3 handleStartPos;
-	private float originOffset;
+	private Vector3 wrldTouch; // our screen youch location translated into world position (screenToWorldPosition)
+	public float wrldTouchDepth = 11.2f; // the depth to set our world touch location (for fiddling)
+	public Camera camCam; // preferably the inworldUIcamera
+	private Vector3 handleStartPos; // where our handle is when scene is loaded
+	private Vector3 menuStartPos; // where our menu is when scene is loaded
+	private float originOffset; // the offset of our touch location wrt to origin of model
+	public float maxHandlePullPos = 24; // the furthest location that the handle can be pulled to (a global position, not an offset)
 
-	public static bool pullingDownMenuHandle = false;
 
-	private float touchStartPosY;
-	private Vector3 moveHandleThisFrame;
-	private Vector3 moveMenuThisFrame;
-	private float menuStartPosZ;
+	private float handlePullScalar; // how far down we have pulled the handle as a 0.00f - 1.00f
+
+	public static bool pullingDownMenuHandle = false; // if we are pulling the handle or not
+
 	// Use this for initialization
 	void Start () {
-		moveHandleThisFrame = transform.localPosition;
-		moveMenuThisFrame = menuItself.transform.localPosition;
-		menuStartPosZ = menuItself.transform.position.z;
-
 		handleStartPos = transform.position;
+		menuStartPos = menuItself.transform.position;
 	}
 
 	// Update is called once per frame
 	void Update () {
 		
 		if (pullingDownMenuHandle) {
-			/*moveHandleThisFrame.z = Input.mousePosition.y/1000;
-			if (moveHandleThisFrame.z < handleUpperLimit && moveHandleThisFrame.z > handleLowerLimit) { // clamp the pulldown range of the handle
-				transform.localPosition = moveHandleThisFrame;
-				moveMenuThisFrame.z = menuOffset  + ((Input.mousePosition.y / 1000) * menuPullRatio);
-				menuItself.transform.localPosition = moveMenuThisFrame;
-			}*/
 
-			wrldTouch = camCam.ScreenToWorldPoint (new Vector3 (0f, Input.mousePosition.y, 11.55f));
+			// calculate where we are in the pull
+			handlePullScalar = (handleStartPos.y - transform.position.y) / (handleStartPos.y - maxHandlePullPos);
+
+			// tell the menu itself to be somewhere in relation to the handle pull
+			// max pos (1.0 scalar) = 31.3
+			// min pos (0.0 scalar) = 43.3
+			// range = 31.3 - 43.3 = -12.0
+			// these are just values I have pulled out of the inspector
+			float menuNewPos = menuStartPos.y + (handlePullScalar * -12f);
+			menuItself.transform.position = new Vector3(menuStartPos.x, menuNewPos, menuStartPos.z);
+
+			// get world position for touch
+			wrldTouch = camCam.ScreenToWorldPoint (new Vector3 (0f, Input.mousePosition.y, wrldTouchDepth));
+
+			// fudge the shit because I'm lazy
 			wrldTouch.x = handleStartPos.x;
-			wrldTouch.y += originOffset;
-			if (wrldTouch.y < 24) {
-				wrldTouch.y = 24;
+
+			// account for the offset of the location of the users tap on the object from the objects origin
+			wrldTouch.y += originOffset; 
+
+			// clamp the handles movement
+			if (wrldTouch.y < maxHandlePullPos) {
+				wrldTouch.y = maxHandlePullPos;
 				wrldTouch.z = -141.1199f;
 			}
 			if (wrldTouch.y > handleStartPos.y) {
 				wrldTouch.y = handleStartPos.y;
 				wrldTouch.z = handleStartPos.z;
 			}
+
+			// update handle position
 			transform.position = wrldTouch;
 
 		}
 	}
 
 	void OnMouseDown(){
+		// toggle pulling down handle mode
 		pullingDownMenuHandle = true;
-		touchStartPosY = Input.mousePosition.y;
-		moveHandleThisFrame = transform.localPosition;
-		moveMenuThisFrame = menuItself.transform.localPosition;
 
 		// account for origin offset of handle
-		wrldTouch = camCam.ScreenToWorldPoint (new Vector3 (0f, Input.mousePosition.y, 11.55f));
+		wrldTouch = camCam.ScreenToWorldPoint (new Vector3 (0f, Input.mousePosition.y, wrldTouchDepth));
 		originOffset = transform.position.y - wrldTouch.y;
 	}
 
 	void OnMouseUp(){
+		// toggle pulling down handle mode
 		pullingDownMenuHandle = false;
 	}
 }
